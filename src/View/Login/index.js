355e1c006a5f187e0471c97db2df0458ea88193b
unknown <Administrador@n087030.int.cagece.com.br>
2020-10-30 22:23:07 -0300

    Enviando arquivos

---
+++ src/View/Login/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './login.css';
+import firebase from '../../config/firebase';
+import 'firebase/auth';
+
+function Login() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    function logar() {
+
+        firebase.auth().signInWithEmailAndPassword(email, senha).then(resultado => {
+            alert('Usuário Logado!');
+        }).catch(Error => {
+            alert(Error);
+        });
+    }
+
+    return (
+
+        <div className="login-content d-flex align-itens-center">
+            <form className="form-signin mx-auto">
+                <div className="text-center mb-4">
+                    {/*<img className="#" src="#" alt="" width="72" height="72" /> */}
+                    <h1 className="h3 mb-3 font-weight-normal font-weight-bold">Login</h1>
+                </div>
+
+                <input onChange={(e) => setEmail(e.target.value)} type="email" id="inputEmail" class="form-control my-2" placeholder="Email" />
+                <input onChange={(e) => setSenha(e.target.value)} type="password" id="inputPassword" class="form-control my-3" placeholder="Senha" />
+
+                <button onClick={logar} class="btn btn-lg btn btn-block btn-login font-weight-bold" type="submit">Entrar</button>
+
+                <div className="msg-login  text-center my-3">
+                    <span className="text-success"><strong>Você está conectado! &#128521;</strong></span>
+                    <br></br>
+                    <span className="text-danger"><strong>Verifique se Usuário ou Senha estão corretos! &#128546;</strong></span>
+                </div>
+
+                <div className="opçoes-login mt-2 text-center font-weight-bold">
+                    <a href="#" className="mx-2">Recuperar Senha</a>
+                    <span className="text-secondary">&#9830;</span>
+                    <a href="#" className="mx-2">Quero Cadastrar</a>
+                    <p class="mt-5 mb-3 text-muted text-center">Desenvolvido por develop.Work &copy; 2020</p>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBTA7hyQN-4i0h5S-03Gbd6RwD5QuEdh8w",
+    authDomain: "telefonia-e53dd.firebaseapp.com",
+    databaseURL: "https://telefonia-e53dd.firebaseio.com",
+    projectId: "telefonia-e53dd",
+    storageBucket: "telefonia-e53dd.appspot.com",
+    messagingSenderId: "334368201860",
+    appId: "1:334368201860:web:72391ab468d2cf40abb6d1"
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
